a5355ed81b4739ef7570cedb96b91488918b3058
kumud-sharma <33771500+kumud-sharma@users.noreply.github.com>
2023-06-02 14:32:41 +0530

    Update psscript.ps1

--- Allfiles/Labs/10/psscript.ps1
+++ Allfiles/Labs/10/psscript.ps1
@@ -2,6 +2,5 @@ $env:chocolateyUseWindowsCompression = 'true'
  $env:chocolateyIgnoreRebootDetected = 'true'
   $env:chocolateyVersion = '1.4.0'
  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-  choco feature enable -n allowGlobalConfirmation
-
-choco.exe install microsoft-edge -y
+ choco feature enable -n allowGlobalConfirmation
+ choco.exe install microsoft-edge -y
