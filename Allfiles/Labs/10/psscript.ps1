2fc2ebb98fff0937a0d2598ff31452ea1649b9bf
AMIT KUMAR <akn9050@gmail.com>
2023-06-02 16:05:15 +0530

    Update psscript.ps1

--- Allfiles/Labs/10/psscript.ps1
+++ Allfiles/Labs/10/psscript.ps1
@@ -1,6 +1,21 @@
-$env:chocolateyUseWindowsCompression = 'true'
- $env:chocolateyIgnoreRebootDetected = 'true'
-  $env:chocolateyVersion = '1.4.0'
- Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
- choco feature enable -n allowGlobalConfirmation
- choco.exe install microsoft-edge -y
+Function InstallChocolatey
+{   
+    $env:chocolateyUseWindowsCompression = 'true'
+    $env:chocolateyIgnoreRebootDetected = 'true'
+    $env:chocolateyVersion = '1.4.0'
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    choco feature enable -n allowGlobalConfirmation
+}
+InstallChocolatey
+
+Function InstallEdgeChromium
+{
+    #Download and Install edge
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("http://go.microsoft.com/fwlink/?LinkID=2093437","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
+    sleep 5
+    
+    Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
+    
+}
+InstallEdgeChromium
